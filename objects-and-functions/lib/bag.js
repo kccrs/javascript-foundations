bdb1faff41d7ef96c4736b584eecbf85973a74a6
Casey Cross <hellocaseycross@gmail.com>
2017-04-13 22:09:27 -0600

    All candy tests passing.  Most bag tests passing.

--- objects-and-functions/lib/bag.js
+++ objects-and-functions/lib/bag.js
@@ -1,5 +1,21 @@
-function Bag() {
+var Candy = require('../lib/candy');
 
+function Bag() {
+  this.empty = true;
+  this.count = 0;
+  this.candies = [];
 };
+
+Bag.prototype.push = function(candy) {
+  this.candies.push(candy);
+  this.count += 1;
+}
+
+Bag.prototype.contains = function(type) {
+  console.log(type);
+  console.log(candy);
+  console.log(this.candies);
+  this.candies.includes(candy.type);
+}
 
 module.exports = Bag;
